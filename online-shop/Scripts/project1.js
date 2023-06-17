e97d19da5caf9eb6bbd2333822393124368ec1eb
hadisjamali <132214893+Hadis-jamali@users.noreply.github.com>
2023-06-18 00:00:12 +0200

    Add files via upload

---
+++ online-shop/Scripts/project1.js
@@ -0,0 +1,16 @@
+// const accordion = document.querySelector('.accordion');
+// const panel = document.querySelector('.panel');
+
+// accordion.addEventListener("click", (event) => {
+//     panel.classList.toggle('active');
+//     if (panel.style.maxHeight) {
+//         panel.style.maxHeight = null
+//     }
+//     else {
+//         panel.style.maxHeight = 100 + 'px';
+//     }
+// });
+
+
+
+
